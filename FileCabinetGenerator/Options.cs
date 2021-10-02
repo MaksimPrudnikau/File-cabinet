fcff3796604b9ecf0fe2ce378e81fcfb0543f92c
Maksim Prudnikau <maksimprudnikau@gmail.com>
2021-10-02 22:07:13 +0300

    fix supporting shortForm command options

--- FileCabinetGenerator/Options.cs
+++ FileCabinetGenerator/Options.cs
@@ -36,36 +36,61 @@ namespace FileCabinetGenerator
 
         private void ParseOutputType(IReadOnlyList<string> args)
         {
-            const int outputTypeIndex = 0;
-            Type = args[outputTypeIndex].StartsWith("--")
-                ? (OutputType) Enum.Parse(typeof(OutputType), GetLongFormValue(args[outputTypeIndex]))
-                : (OutputType) Enum.Parse(typeof(OutputType), args[outputTypeIndex + 1].ToLowerInvariant());
+            const int fullFormIndex = 0;
+            const int shortFormIndex = 1;
+
+            if (IsFullForm(args[fullFormIndex]))
+            {
+                Type = (OutputType) Enum.Parse(typeof(OutputType), GetLongFormValue(args[fullFormIndex]));
+            }
+
+            Type = (OutputType) Enum.Parse(typeof(OutputType), args[shortFormIndex].ToLowerInvariant());
         }
 
         private void ParseOutputFileName(IReadOnlyList<string> args)
         {
-            const int nameIndex = 1;
-            FileName = args[nameIndex].StartsWith("--") 
-                ? GetLongFormValue(args[nameIndex]) 
-                : args[nameIndex + 1].ToLowerInvariant();
+            const int fullFormIndex = 1;
+            const int shortFormIndex = 3;
+            
+            if (IsFullForm(args[fullFormIndex]))
+            {
+                FileName = GetLongFormValue(args[fullFormIndex]);
+            }
+            
+            FileName = args[shortFormIndex].ToLowerInvariant();
         }
         
         private void ParseRecordsAmount(IReadOnlyList<string> args)
         {
-            const int recordsAmountIndex = 2;
-            Count = args[recordsAmountIndex].StartsWith("--")
-                ? Convert.ToInt64(GetLongFormValue(args[recordsAmountIndex]))
-                : Convert.ToInt64(args[recordsAmountIndex + 1].ToLowerInvariant());
+            const int fullFormIndex = 2;
+            const int shortFormIndex = 5;
+
+            if (args[fullFormIndex].StartsWith("--"))
+            {
+                Count = Convert.ToInt64(GetLongFormValue(args[fullFormIndex]));
+            }
+            
+            Count = Convert.ToInt64(args[shortFormIndex].ToLowerInvariant());
         }
         
         private void ParseStartId(IReadOnlyList<string> args)
         {
-            const int recordsAmountIndex = 3;
-            StartId = args[recordsAmountIndex].StartsWith("--")
-                ? Convert.ToInt64(GetLongFormValue(args[recordsAmountIndex]))
-                : Convert.ToInt64(args[recordsAmountIndex + 1].ToLowerInvariant());
+            const int fullFormIndex = 4;
+            const int shortFormIndex = 7;
+
+            if (IsFullForm(args[fullFormIndex]))
+            {
+                StartId = Convert.ToInt64(GetLongFormValue(args[fullFormIndex]));
+            }
+            
+            StartId = Convert.ToInt64(args[shortFormIndex].ToLowerInvariant());
         }
 
+        private bool IsFullForm(string parameter)
+        {
+            return parameter.StartsWith("--");
+        }
+        
         private string GetLongFormValue(string parameter)
         {
             var equalSign = parameter.IndexOf('=');
--- FileCabinetGenerator/Program.cs
+++ FileCabinetGenerator/Program.cs
@@ -1,12 +1,10 @@
-﻿using System;
-
+﻿
 namespace FileCabinetGenerator
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
             var options = new Options(args);
         }
     }
