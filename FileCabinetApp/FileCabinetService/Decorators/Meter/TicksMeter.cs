ffe957b2c0df4844efe14e96557cd840771c0081
Maksim Prudnikau <maksimprudnikau@gmail.com>
2021-11-19 19:29:46 +0300

    add xml-doc

--- FileCabinetApp/FileCabinetService/Decorators/Meter/TicksMeter.cs
+++ FileCabinetApp/FileCabinetService/Decorators/Meter/TicksMeter.cs
@@ -7,6 +7,16 @@ namespace FileCabinetApp.FileCabinetService.Decorators.Meter
     {
         private static readonly Stopwatch Stopwatch = new();
         
+        /// <summary>
+        /// Get the method's execution time in elapsed ticks
+        /// </summary>
+        /// <param name="method">Delegate to the method to invoke</param>
+        /// <param name="parameter">Method's parameter</param>
+        /// <param name="methodsOut">Method's return value</param>
+        /// <typeparam name="TIn">Input parameter type</typeparam>
+        /// <typeparam name="TOut">Method's return value type</typeparam>
+        /// <returns>Method's execution time in elapsed ticks</returns>
+        /// <exception cref="ArgumentNullException">The delegate is null</exception>
         public static long GetElapsedTicks<TIn, TOut>(Func<TIn, TOut> method, TIn parameter, out TOut methodsOut)
         {
             if (method is null)
@@ -23,6 +33,14 @@ namespace FileCabinetApp.FileCabinetService.Decorators.Meter
             return ticks;
         }
         
+        /// <summary>
+        /// Get the method's execution time in elapsed ticks
+        /// </summary>
+        /// <param name="method">Delegate to the method to invoke</param>
+        /// <param name="methodsOut">Method's return value</param>
+        /// <typeparam name="T">Method's return value type</typeparam>
+        /// <returns>Method's execution time in elapsed ticks</returns>
+        /// <exception cref="ArgumentNullException">The delegate is null</exception>
         public static long GetElapsedTicks<T>(Func<T> method, out T methodsOut)
         {
             if (method is null)
@@ -39,6 +57,14 @@ namespace FileCabinetApp.FileCabinetService.Decorators.Meter
             return ticks;
         }
         
+        /// <summary>
+        /// Get the method's execution time in elapsed ticks
+        /// </summary>
+        /// <param name="method">Delegate to the method to invoke</param>
+        /// <param name="parameter">Method's parameter</param>
+        /// <typeparam name="T">Method's return value type</typeparam>
+        /// <returns>Method's execution time in elapsed ticks</returns>
+        /// <exception cref="ArgumentNullException">The delegate is null</exception>
         public static long GetElapsedTicks<T>(Action<T> method, T parameter)
         {
             if (method is null)
@@ -55,6 +81,12 @@ namespace FileCabinetApp.FileCabinetService.Decorators.Meter
             return ticks;
         }
         
+        /// <summary>
+        /// Get the method's execution time in elapsed ticks
+        /// </summary>
+        /// <param name="method">Delegate to the method to invoke</param>
+        /// <returns>Method's execution time in elapsed ticks</returns>
+        /// <exception cref="ArgumentNullException">The delegate is null</exception>
         public static long GetElapsedTicks(Action method)
         {
             if (method is null)
@@ -70,6 +102,18 @@ namespace FileCabinetApp.FileCabinetService.Decorators.Meter
             return ticks;
         }
         
+        /// <summary>
+        /// Get the method's execution time in elapsed ticks
+        /// </summary>
+        /// <param name="method">Delegate to the method to invoke</param>
+        /// <param name="first">The first method's parameter</param>
+        /// <param name="second">The second method's parameter</param>
+        /// <param name="methodsOut">The method's return value</param>
+        /// <typeparam name="T1In">The first method's parameter type</typeparam>
+        /// <typeparam name="T2In">The second method's parameter type</typeparam>
+        /// <typeparam name="TOut">The method's return value type</typeparam>
+        /// <returns>Method's execution time in elapsed ticks</returns>
+        /// <exception cref="ArgumentNullException">The delegate is null</exception>
         public static long GetElapsedTicks<T1In, T2In, TOut>(Func<T1In, T2In, TOut> method, T1In first, T2In second, out TOut methodsOut)
         {
             if (method is null)
