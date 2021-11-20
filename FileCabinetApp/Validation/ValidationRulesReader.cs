094162b0f84be6686e6cedd3f4ce248128659a87
Maksim Prudnikau <maksimprudnikau@gmail.com>
2021-11-20 13:21:35 +0300

    improve exceptions handling and add xml-doc

--- FileCabinetApp/Validation/ValidationRulesReader.cs
+++ FileCabinetApp/Validation/ValidationRulesReader.cs
@@ -7,6 +7,11 @@ namespace FileCabinetApp.Validation
 {
     public static class ValidationRulesReader
     {
+        /// <summary>
+        /// Read <see cref="ValidationRules"/> from JSON file
+        /// </summary>
+        /// <param name="path">Absolute path to the file with rules</param>
+        /// <returns><see cref="ValidationRules"/> object</returns>
         public static ValidationRules ReadRules(string path)
         {
             try
@@ -17,11 +22,16 @@ namespace FileCabinetApp.Validation
             }
             catch (SystemException e) when (e is ArgumentException or IOException)
             {
-                Console.WriteLine(e);
-                throw;
+                Console.Error.WriteLine(e.Message);
+                return null;
             }
         }
 
+        /// <summary>
+        /// Fill null criterias of the source validation rules with their default values
+        /// </summary>
+        /// <param name="source">Source validation rule</param>
+        /// <returns>Filled <see cref="ValidationRules"/> object</returns>
         private static ValidationRules FillNullParameters(ValidationRules source)
         {
             FillNullDefaultParameters(source);
@@ -29,6 +39,10 @@ namespace FileCabinetApp.Validation
             return source;
         }
 
+        /// <summary>
+        /// Fill null default criterias from source <see cref="ValidationRules"/>
+        /// </summary>
+        /// <param name="source">Source rules</param>
         private static void FillNullDefaultParameters(ValidationRules source)
         {
             source.Default.FirstName ??= new Name();
@@ -36,6 +50,10 @@ namespace FileCabinetApp.Validation
             source.Default.DateOfBirth ??= new DateOfBirth();
         }
         
+        /// <summary>
+        /// Fill null custom criterias from source <see cref="ValidationRules"/>
+        /// </summary>
+        /// <param name="source">Source rules</param>
         private static void FillNullCustomParameters(ValidationRules source)
         {
             source.Custom.FirstName ??= new Name();
